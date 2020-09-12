1af4257ae0722d57efafd3c840539461aeea1345
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-09-11 22:06:18 -0300

    Typos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name='students-repos-handler',
+    name='student-repos-handler',
     version='1.0.0',
     description='Simple tool to handle multiple repos at once, oriented to the grading of assignments in UCSE DAR',
     long_description=open('README.md').read(),
